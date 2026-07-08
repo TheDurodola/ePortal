f4a9ab8c15816759ed8a79f2f20d76c4de614006
Durodola Abolaji <bolajidurodola@gmail.com>
2026-07-08 11:53:20 +0100

    set up: folder structure up

---
+++ src/test/java/com/school/eportal/EportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.eportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
